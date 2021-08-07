bf033c7726d49d5ab029538eb12bc077ef13b839
Stanley Wintergreen <kepocnhh@gmail.com>
2021-08-07 19:08:43 +0300

    0.0.7

--- lib/src/main/kotlin/sp/kx/okhttp/RequestUtil.kt
+++ lib/src/main/kotlin/sp/kx/okhttp/RequestUtil.kt
@@ -2,6 +2,7 @@ package sp.kx.okhttp
 
 import okhttp3.HttpUrl
 import okhttp3.Request
+import okhttp3.RequestBody
 
 fun requestBuilder(builder: Request.Builder.() -> Unit): Request.Builder {
     return Request.Builder().also(builder)
@@ -17,17 +18,45 @@ fun requestBuilder(httpUrl: HttpUrl, headers: Map<String, String>): Request.Buil
 }
 
 fun requestBuilder(
+    httpUrl: HttpUrl,
+    headers: Map<String, String>,
+    method: Method,
+    body: RequestBody
+): Request.Builder {
+    return requestBuilder {
+        url(httpUrl)
+        headers.forEach { (key, value) ->
+            addHeader(key, value)
+        }
+        when (method) {
+            Method.POST -> post(body)
+        }
+    }
+}
+
+fun requestBuilder(
     url: String,
     queries: Map<String, String>,
     headers: Map<String, String>
 ): Request.Builder {
     return requestBuilder(
-        httpUrl = httpUrl(url) {
-            queries.forEach { (key, value) ->
-                addQueryParameter(key, value)
-            }
-        },
+        httpUrl = httpUrl(url = url, queries = queries),
         headers = headers
+    )
+}
+
+fun requestBuilder(
+    url: String,
+    queries: Map<String, String>,
+    headers: Map<String, String>,
+    method: Method,
+    body: RequestBody
+): Request.Builder {
+    return requestBuilder(
+        httpUrl = httpUrl(url = url, queries = queries),
+        headers = headers,
+        method = method,
+        body = body
     )
 }
 
@@ -45,6 +74,22 @@ fun request(
     headers: Map<String, String>
 ): Request {
     return requestBuilder(url = url, queries = queries, headers = headers).build()
+}
+
+fun request(
+    url: String,
+    queries: Map<String, String>,
+    headers: Map<String, String>,
+    method: Method,
+    body: RequestBody
+): Request {
+    return requestBuilder(
+        url = url,
+        queries = queries,
+        headers = headers,
+        method = method,
+        body = body
+    ).build()
 }
 
 fun Request.newBuilder(builder: Request.Builder.() -> Unit): Request.Builder {
--- lib/src/test/kotlin/sp/kx/okhttp/RequestUtilTest.kt
+++ lib/src/test/kotlin/sp/kx/okhttp/RequestUtilTest.kt
@@ -1,12 +1,13 @@
 package sp.kx.okhttp
 
-import java.net.URI
 import java.net.URL
 import okhttp3.HttpUrl.Companion.toHttpUrl
 import okhttp3.Request
-import org.junit.jupiter.api.Test
+import okhttp3.RequestBody.Companion.toRequestBody
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
 
 internal class RequestUtilTest {
     @Test
@@ -57,6 +58,29 @@ internal class RequestUtilTest {
         assertEquals(requestNew.headers[keyNew], valueNew)
     }
 
+    private fun assertSame(expected: URL, actual: URL) {
+        assertEquals(expected.host, actual.host)
+        assertEquals(expected.protocol, actual.protocol)
+    }
+
+    private fun assertQueries(queries: Map<String, String>, request: Request) {
+        assertEquals(request.url.queryParameterNames.size, queries.keys.size)
+        queries.forEach { (key, value) ->
+            val values = request.url.queryParameterValues(key)
+            assertEquals(values.size, 1)
+            assertEquals(values.first(), value)
+        }
+    }
+
+    private fun assertHeaders(headers: Map<String, String>, request: Request) {
+        assertEquals(request.headers.size, headers.size)
+        headers.forEach { (key, value) ->
+            val values = request.headers(key)
+            assertEquals(values.size, 1)
+            assertEquals(values.first(), value)
+        }
+    }
+
     @Test
     fun requestUrlQueriesHeadersTest() {
         val url = "https://github.com/"
@@ -67,20 +91,54 @@ internal class RequestUtilTest {
             queries = queries,
             headers = headers
         )
-        val urlExpected = URL(url)
-        assertEquals(request.url.toUrl().host, urlExpected.host)
-        assertEquals(request.url.toUrl().protocol, urlExpected.protocol)
-        assertEquals(request.url.queryParameterNames.size, queries.keys.size)
-        queries.forEach { (key, value) ->
-            val values = request.url.queryParameterValues(key)
-            assertEquals(values.size, 1)
-            assertEquals(values.first(), value)
+        assertSame(expected = URL(url), actual = request.url.toUrl())
+        assertQueries(queries = queries, request = request)
+        assertHeaders(headers = headers, request = request)
+    }
+
+    @Test
+    fun requestBuilderUrlQueriesHeadersBodyTest() {
+        val url = "https://github.com/"
+        val queries = mapOf("foo" to "bar")
+        val headers = mapOf("baz" to "qux")
+        val method = Method.POST
+        val body = "body".toRequestBody()
+        val request = requestBuilder(
+            url = url,
+            queries = queries,
+            headers = headers,
+            method = method,
+            body = body
+        ).build()
+        assertSame(expected = URL(url), actual = request.url.toUrl())
+        assertQueries(queries = queries, request = request)
+        assertHeaders(headers = headers, request = request)
+        assertEquals(request.method, method.name)
+        assertTrue(request.body === body) {
+            "It is not the same object!"
         }
-        assertEquals(request.headers.size, headers.size)
-        headers.forEach { (key, value) ->
-            val values = request.headers(key)
-            assertEquals(values.size, 1)
-            assertEquals(values.first(), value)
+    }
+
+    @Test
+    fun requestUrlQueriesHeadersBodyTest() {
+        val url = "https://github.com/"
+        val queries = mapOf("foo" to "bar")
+        val headers = mapOf("baz" to "qux")
+        val method = Method.POST
+        val body = "body".toRequestBody()
+        val request = request(
+            url = url,
+            queries = queries,
+            headers = headers,
+            method = method,
+            body = body
+        )
+        assertSame(expected = URL(url), actual = request.url.toUrl())
+        assertQueries(queries = queries, request = request)
+        assertHeaders(headers = headers, request = request)
+        assertEquals(request.method, method.name)
+        assertTrue(request.body === body) {
+            "It is not the same object!"
         }
     }
 }
