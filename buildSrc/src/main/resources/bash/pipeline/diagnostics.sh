7d2d94190611728357e2ed4d7e49b415a86e91b9
Stanley Wintergreen <kepocnhh@gmail.com>
2022-03-26 14:59:23 +0300

    svc | ci | 1648295963

---
+++ buildSrc/src/main/resources/bash/pipeline/diagnostics.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "Pipeline diagnostics start..."
+
+mkdir -p diagnostics || exit 1 # todo
+echo "{\"types\":[]}" > diagnostics/summary.json
+
+DIAGNOSTICS_FILE=repository/buildSrc/src/main/resources/json/diagnostics.json
+ARRAY=($(jq -Mcer ".|keys|.[]" $DIAGNOSTICS_FILE))
+SIZE=${#ARRAY[*]}
+for ((i=0; i<SIZE; i++)); do
+ TYPE="${ARRAY[i]}"
+ CODE=0
+ gradle -p repository $(jq -Mcer ".${TYPE}.task" $DIAGNOSTICS_FILE); CODE=$?
+ if test $CODE -ne 0; then
+  mkdir -p diagnostics/report/$TYPE || exit 1 # todo
+  cp repository/build/reports/$(jq -Mcer ".${TYPE}.report" $DIAGNOSTICS_FILE) diagnostics/report/$TYPE/index.html || exit $((100+i))
+  echo "$(jq -cM ".types+=[\"$TYPE\"]" diagnostics/summary.json)" > diagnostics/summary.json || exit $((100+i))
+ fi
+done
+
+TYPES="$(jq -Mcer .types diagnostics/summary.json)" || exit 1 # todo
+if test "$TYPES" == "[]"; then
+ echo "Diagnostics should have determined the cause of the failure!"; exit 1
+fi
+
+echo "Diagnostics have determined the cause of the failure - this is: $TYPES."
+
+exit 0
