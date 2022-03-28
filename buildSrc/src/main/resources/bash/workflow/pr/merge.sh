945ad082f46ec60c07b82f13cd0d18fb6402ecdd
Stanley Wintergreen <kepocnhh@gmail.com>
2022-03-28 19:40:20 +0300

    svc | ci | 1648485620

--- buildSrc/src/main/resources/bash/workflow/pr/merge.sh
+++ buildSrc/src/main/resources/bash/workflow/pr/merge.sh
@@ -13,18 +13,23 @@ GIT_COMMIT_SRC="$(jq -r .head.sha assemble/vcs/pr${PR_NUMBER}.json)"
 for it in WORKER_NAME WORKER_EMAIL GIT_BRANCH_DST GIT_COMMIT_SRC; do
  if test -z "${!it}"; then echo "$it is empty!"; exit 11; fi; done
 
+CODE=0
 git -C repository config user.name "$WORKER_NAME" && \
  git -C repository config user.email "$WORKER_EMAIL"; CODE=$?
 if test $CODE -ne 0; then
  echo "Git config error!"; exit 101
 fi
+git -C repository fetch origin $GIT_BRANCH_DST; CODE=$?
+if test $CODE -ne 0; then
+ echo "Git fetch from origin \"$GIT_BRANCH_DST\" error!"; exit 102
+fi
 git -C repository checkout $GIT_BRANCH_DST; CODE=$?
 if test $CODE -ne 0; then
- echo "Git checkout to \"$GIT_BRANCH_DST\" error!"; exit 102
+ echo "Git checkout to \"$GIT_BRANCH_DST\" error!"; exit 103
 fi
 git -C repository merge --no-ff --no-commit $GIT_COMMIT_SRC; CODE=$?
 if test $CODE -ne 0; then
- echo "Git merge with ${GIT_COMMIT_SRC::7} failed!"; exit 103
+ echo "Git merge with ${GIT_COMMIT_SRC::7} failed!"; exit 104
 fi
 
 echo "Workflow merge ${GIT_COMMIT_SRC::7} -> $GIT_BRANCH_DST finish."
